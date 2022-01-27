4567788644e3cf1087b15250d84e50d70341bbbd
Burak Halefoğlu <42536490+burakhalefoglu@users.noreply.github.com>
2022-01-27 21:18:55 +0300

    Update connection.go

--- database/mongodb/connection.go
+++ database/mongodb/connection.go
@@ -2,11 +2,9 @@ package mongodb
 
 import (
 	"context"
-	"os"
-	"time"
-
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"os"
 )
 
 type Connection struct {
@@ -15,13 +13,11 @@ type Connection struct {
 
 var Conn = Connection{}
 
-func ConnectMongodb()(*mongo.Client){
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	var url = "mongodb://" + os.Getenv("MONGODB_USER") + ":" + os.Getenv("MONGODB_PASS") + "@" + os.Getenv("MONGODB_HOST") + ":" + os.Getenv("MONGODB_PORT")
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(url))
-	if err != nil{
+func ConnectMongodb() *mongo.Client {
+	var uri = "mongodb://" + os.Getenv("MONGODB_USER") + ":" + os.Getenv("MONGODB_PASS") + "@" + os.Getenv("MONGODB_HOST") + ":" + os.Getenv("MONGODB_PORT") + "/?maxPoolSize=20&w=majority"
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
 		panic(err)
 	}
-	return  client
+	return client
 }
